1c74c4833b591771ab748e108b36f670becccc3d
Ariel-ksenzovsky <a.ksenzovsky@gmail.com>
2025-01-23 14:58:04 +0200

    test no.2

--- fetch-secrets.sh
+++ fetch-secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fetch secret from AWS Secrets Manager
-secret=$(aws secretsmanager get-secret-value --secret-id my-secret --query SecretString --output text)
+secret=$(aws secretsmanager get-secret-value --secret-id .env --region us-east-1 --query SecretString --output text)
 
 # Export secrets as environment variables
 export DB_HOST=$(echo $secret | jq -r .DB_HOST)
@@ -10,4 +10,4 @@ export DB_PASSWORD=$(echo $secret | jq -r .DB_PASSWORD)
 export DB_NAME=$(echo $secret | jq -r .DB_NAME)
 export FLASK_PORT=$(echo $secret | jq -r .FLASK_PORT)
 
-exec "$@"
+docker-compose up --build
