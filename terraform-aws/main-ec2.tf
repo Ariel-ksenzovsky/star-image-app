67ad2b781eeea32e8e26406da049609fc7e66b9f
Ariel-ksenzovsky <a.ksenzovsky@gmail.com>
2025-01-29 10:33:15 +0200

    backend test

--- terraform-aws/main-ec2.tf
+++ terraform-aws/main-ec2.tf
@@ -1,90 +1,90 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "my-bucket101110101"  # Your existing S3 bucket for state and .env
-#     key    = "terraform/terraform.tfstate"  # Path for the Terraform state file
-#     region = "us-east-1"  # Your AWS region
-#     encrypt = true  # Enable encryption for the state file
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "my-bucket101110101"  # Your existing S3 bucket for state and .env
+    key    = "terraform/terraform.tfstate"  # Path for the Terraform state file
+    region = "us-east-1"  # Your AWS region
+    encrypt = true  # Enable encryption for the state file
+  }
+}
 
-# provider "aws" {
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region = "us-east-1"
+}
 
-# # Generate a new SSH key pair
-# resource "tls_private_key" "example" {
-#   algorithm = "RSA"
-#   rsa_bits  = 2048
-# }
+# Generate a new SSH key pair
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
 
-# # Create an AWS key pair using the generated public key
-# resource "aws_key_pair" "key_pair" {
-#   key_name   = "instance-test-key"
-#   public_key = tls_private_key.example.public_key_openssh
-# }
+# Create an AWS key pair using the generated public key
+resource "aws_key_pair" "key_pair" {
+  key_name   = "instance-test-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
 
-# # Create the security group with necessary ports
-# resource "aws_security_group" "sg" {
-#   name_prefix = "terraform-sg-"
+# Create the security group with necessary ports
+resource "aws_security_group" "sg" {
+  name_prefix = "terraform-sg-"
 
-#   ingress {
-#     from_port   = 3306
-#     to_port     = 3306
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 5000
-#     to_port     = 5000
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# # Create the EC2 instance and associate the existing IAM role
-# resource "aws_instance" "docker_instance" {
-#   ami             = "ami-0df8c184d5f6ae949"  # Replace with your desired AMI ID
-#   instance_type   = "t2.micro"
-#   key_name        = aws_key_pair.key_pair.key_name
-#   security_groups = [aws_security_group.sg.name]
+# Create the EC2 instance and associate the existing IAM role
+resource "aws_instance" "docker_instance" {
+  ami             = "ami-0df8c184d5f6ae949"  # Replace with your desired AMI ID
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.key_pair.key_name
+  security_groups = [aws_security_group.sg.name]
 
-#   iam_instance_profile = "s3Access" # Reference the existing IAM instance profile directly
+  iam_instance_profile = "s3Access" # Reference the existing IAM instance profile directly
 
-#   user_data = <<-EOF
-#               #!/bin/bash
+  user_data = <<-EOF
+              #!/bin/bash
 
-#               # Update the system and install Ansible
-#               sudo yum update -y
-#               sudo yum install -y git ansible
+              # Update the system and install Ansible
+              sudo yum update -y
+              sudo yum install -y git ansible
 
-#               # Clone the repository containing the Ansible playbook
-#               git clone https://github.com/Ariel-ksenzovsky/star-image-app.git /home/ec2-user/star-image-app
+              # Clone the repository containing the Ansible playbook
+              git clone https://github.com/Ariel-ksenzovsky/star-image-app.git /home/ec2-user/star-image-app
 
-#               # Run the Ansible playbook
-#               ansible-playbook /home/ec2-user/star-image-app/ansible-flask-app.yml
-#               EOF
+              # Run the Ansible playbook
+              ansible-playbook /home/ec2-user/star-image-app/ansible-flask-app.yml
+              EOF
 
-#   tags = {
-#     Name = "FlaskAppInstance"
-#   }
-# }
+  tags = {
+    Name = "FlaskAppInstance"
+  }
+}
 
-# # Output the public IP of the created EC2 instance
-# output "public_ip" {
-#   value = aws_instance.docker_instance.public_ip
-# }
+# Output the public IP of the created EC2 instance
+output "public_ip" {
+  value = aws_instance.docker_instance.public_ip
+}
--- terraform-azure/main-azure-aks.tf
+++ terraform-azure/main-azure-aks.tf
@@ -1,106 +1,106 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "a483b7fe-d92b-4c77-bb64-5e57db091e39" # Replace with your subscription ID
-}
+# provider "azurerm" {
+#   features {}
+#   subscription_id = "a483b7fe-d92b-4c77-bb64-5e57db091e39" # Replace with your subscription ID
+# }
 
-terraform {
-  backend "s3" {
-    bucket         = "my-bucket101110101"
-    key            = "azure-aks-terraform/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "my-bucket101110101"
+#     key            = "azure-aks-terraform/terraform.tfstate"
+#     region         = "us-east-1"
+#     encrypt        = true
+#   }
+# }
 
-resource "azurerm_kubernetes_cluster" "flask-project" {
-  name                = "flask-project"       # Kubernetes cluster name
-  location            = "East US"             # Your preferred Azure region
-  resource_group_name = "rg-kubernetes"      
-  dns_prefix         = "flask-project-dns"    # DNS prefix for the cluster
-  automatic_upgrade_channel = "patch"
-  azure_policy_enabled = false
-  cost_analysis_enabled = false
-  http_application_routing_enabled = false
-  image_cleaner_enabled = true
-  image_cleaner_interval_hours = 168
-  local_account_disabled = false
-  oidc_issuer_enabled = true
-  open_service_mesh_enabled = false
-  tags = {}
-  auto_scaler_profile {
-    balance_similar_node_groups = false
-    daemonset_eviction_for_empty_nodes_enabled = false
-    daemonset_eviction_for_occupied_nodes_enabled = true
-    empty_bulk_delete_max = "10"
-    expander                   = "random"
-    max_graceful_termination_sec = 600
-    max_node_provisioning_time = "15m"
-    max_unready_nodes = 3
-    max_unready_percentage = 45
-    scale_down_delay_after_add = "10m"
-    scale_down_delay_after_delete = "10s"
-    scale_down_delay_after_failure = "3m"
-    scale_down_unneeded = "10m"
-    scale_down_unready = "10m"
-    scale_down_utilization_threshold = "0.5"
-    scan_interval = "10s"
-    skip_nodes_with_local_storage = false
-    skip_nodes_with_system_pods = true
-  }
+# resource "azurerm_kubernetes_cluster" "flask-project" {
+#   name                = "flask-project"       # Kubernetes cluster name
+#   location            = "East US"             # Your preferred Azure region
+#   resource_group_name = "rg-kubernetes"      
+#   dns_prefix         = "flask-project-dns"    # DNS prefix for the cluster
+#   automatic_upgrade_channel = "patch"
+#   azure_policy_enabled = false
+#   cost_analysis_enabled = false
+#   http_application_routing_enabled = false
+#   image_cleaner_enabled = true
+#   image_cleaner_interval_hours = 168
+#   local_account_disabled = false
+#   oidc_issuer_enabled = true
+#   open_service_mesh_enabled = false
+#   tags = {}
+#   auto_scaler_profile {
+#     balance_similar_node_groups = false
+#     daemonset_eviction_for_empty_nodes_enabled = false
+#     daemonset_eviction_for_occupied_nodes_enabled = true
+#     empty_bulk_delete_max = "10"
+#     expander                   = "random"
+#     max_graceful_termination_sec = 600
+#     max_node_provisioning_time = "15m"
+#     max_unready_nodes = 3
+#     max_unready_percentage = 45
+#     scale_down_delay_after_add = "10m"
+#     scale_down_delay_after_delete = "10s"
+#     scale_down_delay_after_failure = "3m"
+#     scale_down_unneeded = "10m"
+#     scale_down_unready = "10m"
+#     scale_down_utilization_threshold = "0.5"
+#     scan_interval = "10s"
+#     skip_nodes_with_local_storage = false
+#     skip_nodes_with_system_pods = true
+#   }
 
-  default_node_pool {
-    auto_scaling_enabled = true
-    fips_enabled = false
-    host_encryption_enabled = false
-    kubelet_disk_type = "OS"
-    max_count = 5
-    max_pods = 110
-    min_count = 2
-    node_labels = {}
-    node_public_ip_enabled = false
-    only_critical_addons_enabled = false
-    tags = {}
-    zones = []
-    name       = "agentpool"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
-    os_disk_size_gb = 128
-    os_sku = "Ubuntu"
-    upgrade_settings {
-      drain_timeout_in_minutes = 0
-      max_surge = "10%"
-      node_soak_duration_in_minutes = 0
-    }
-  }
+#   default_node_pool {
+#     auto_scaling_enabled = true
+#     fips_enabled = false
+#     host_encryption_enabled = false
+#     kubelet_disk_type = "OS"
+#     max_count = 5
+#     max_pods = 110
+#     min_count = 2
+#     node_labels = {}
+#     node_public_ip_enabled = false
+#     only_critical_addons_enabled = false
+#     tags = {}
+#     zones = []
+#     name       = "agentpool"
+#     node_count = 2
+#     vm_size    = "Standard_DS2_v2"
+#     os_disk_size_gb = 128
+#     os_sku = "Ubuntu"
+#     upgrade_settings {
+#       drain_timeout_in_minutes = 0
+#       max_surge = "10%"
+#       node_soak_duration_in_minutes = 0
+#     }
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  maintenance_window_auto_upgrade {
-    day_of_month = 0
-    day_of_week  = "Sunday"
-    duration     = 4
-    frequency    = "Weekly"
-    interval     = 1
-    start_date = "2025-01-26T00:00:00Z"
-    start_time = "00:00"
-    utc_offset = "+00:00"
-  }
+#   maintenance_window_auto_upgrade {
+#     day_of_month = 0
+#     day_of_week  = "Sunday"
+#     duration     = 4
+#     frequency    = "Weekly"
+#     interval     = 1
+#     start_date = "2025-01-26T00:00:00Z"
+#     start_time = "00:00"
+#     utc_offset = "+00:00"
+#   }
 
-  maintenance_window_node_os {
-    day_of_month = 0
-    day_of_week  = "Sunday"
-    duration     = 4
-    frequency    = "Weekly"
-    interval     = 1
-    start_date = "2025-01-26T00:00:00Z"
-    start_time = "00:00"
-    utc_offset = "+00:00"
-  }
+#   maintenance_window_node_os {
+#     day_of_month = 0
+#     day_of_week  = "Sunday"
+#     duration     = 4
+#     frequency    = "Weekly"
+#     interval     = 1
+#     start_date = "2025-01-26T00:00:00Z"
+#     start_time = "00:00"
+#     utc_offset = "+00:00"
+#   }
 
-}
+# }
 
-  output "client_id" {
-    value = azurerm_kubernetes_cluster.flask-project.identity[0].principal_id
-  }
+#   output "client_id" {
+#     value = azurerm_kubernetes_cluster.flask-project.identity[0].principal_id
+#   }
